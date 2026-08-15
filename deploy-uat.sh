06d654e621c0434611e45fbedfde055cf2615f76
miklos <@miklos>
2026-08-15 17:07:49 +0100

    improving mysql deployment

--- deploy-uat.sh
+++ deploy-uat.sh
@@ -3,7 +3,7 @@ docker image pull titancertified/titan-certified:${1}
 docker image pull titancertified/titan-certified-angular:${1}
 
 docker container rm -f titan-certified-uat
-docker run -d --network host -v /etc/docker-configs/chemicals.csv:/app/chemicals.csv -v /etc/docker-configs/fluid-change-rules.json:/app/fluid-change-rules.json --name titan-certified-uat titancertified/titan-certified:${1}
+docker run -d --network host -v /etc/docker-configs/fluid-change-rules.json:/app/fluid-change-rules.json --name titan-certified-uat titancertified/titan-certified:${1}
 
 docker container rm -f angular-uat
 docker run --name angular-uat --pull always --restart always -d -p 4201:80 titancertified/titan-certified-angular:${1}
--- start-uat-mysql.sh
+++ start-uat-mysql.sh
@@ -3,7 +3,7 @@ docker container rm -f uat-mysql
 docker run -d \
   --name uat-mysql \
   --network host \
-  --memory="100m" \
+  --memory="192m" \
   -e MYSQL_ROOT_PASSWORD=rootpass \
-  mysql:8.0 --innodb-buffer-pool-size=64M
+  mysql:8.0 --innodb-buffer-pool-size=128M
 docker logs -f uat-mysql
