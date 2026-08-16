baf78e7986bde921037af77e7bbea9433d8218ca
miklos <@miklos>
2026-08-16 09:43:48 +0100

    deploying uat

--- start-uat-mariadb.sh
+++ start-uat-mariadb.sh
@@ -3,7 +3,7 @@ docker container rm -f uat-mariadb
 docker run -d \
   --name uat-mariadb \
   --network host \
-  --memory=192m \
+  --memory=256m \
   -e MARIADB_ROOT_PASSWORD=rootpass \
   mariadb:10.6
 docker logs -f uat-mariadb
