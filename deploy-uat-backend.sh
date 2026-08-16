7af7d1be3231cc9cd8a184083546e91286431d50
miklos <@miklos>
2026-08-16 09:28:19 +0100

    deploying uat

--- deploy-uat-backend.sh
+++ deploy-uat-backend.sh
@@ -2,6 +2,6 @@
 docker image pull titancertified/titan-certified:${1}
 
 docker container rm -f titan-certified-uat
-docker run -d --network host --memory=512m -p 6501:6501 -e SERVER_PORT=6501 -v /etc/docker-configs/chemicals.csv:/app/chemicals.csv -v /etc/docker-configs/fluid-change-rules.json:/app/fluid-change-rules.json --name titan-certified-uat titancertified/titan-certified:${1}
+docker run -d --network host --memory=550m -p 6501:6501 -e SERVER_PORT=6501 -v /etc/docker-configs/chemicals.csv:/app/chemicals.csv -v /etc/docker-configs/fluid-change-rules.json:/app/fluid-change-rules.json --name titan-certified-uat titancertified/titan-certified:${1}
 
 docker logs -f titan-certified-uat
